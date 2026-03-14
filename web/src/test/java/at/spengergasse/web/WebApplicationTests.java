6e2a8dc39ddc902a82e942deff27590e34ec2a0c
Sima Baric <sima.baric0605@gmail.com>
2026-03-14 15:00:28 +0100

    Fixed WebApplicationTest contextLoad test failing

--- web/src/test/java/at/spengergasse/web/WebApplicationTests.java
+++ web/src/test/java/at/spengergasse/web/WebApplicationTests.java
@@ -1,10 +1,15 @@
 package at.spengergasse.web;
 
+import at.spengergasse.web.service.CustomerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 public class WebApplicationTests {
+
+    @MockitoBean
+    private CustomerService customerService;
 
     @Test
     void contextLoads() {}
